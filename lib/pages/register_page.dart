3124d0005f615f86cb5658bb8aa2f6b00dfa7879
Safiya Sajad <safiyasajad@gmail.com>
2026-06-23 12:07:17 +0530

    Update register_page.dart
    allows for email, password and confirm password to be saved to database

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,3 +1,5 @@
+import 'package:authfluttersupabase/auth/auth_service.dart';
+import 'package:authfluttersupabase/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -8,8 +10,86 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  //gets auth service
+  final authService = AuthService();
+
+  //text controller
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+
+//sign up button pressed
+void signUp() async{
+  //prepare data
+  final email = _emailController.text;
+  final password = _passwordController.text;
+  final confirmPassword = _confirmPasswordController.text;
+
+
+  //check passwords match
+  if (password != confirmPassword) {
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Passwords don't match")));
+    return;
+  }
+
+  //attempt sign up
+  try {
+    await authService.signUpWithEmailPassword(email, password);
+
+    //pop this register page
+    Navigator.pop(context); //what is this?
+  } catch (e) {
+    if(mounted) {
+       ScaffoldMessenger.of(context).showSnackBar( SnackBar(content: Text("Error $e")));
+    }
+  }
+
+}
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      //the app bar gives the page a title as well as the back arrow on the left 
+      appBar: AppBar(
+        title: const Text("Sign Up")
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 50),
+        children: [
+          //email 
+          TextField(
+            controller: _emailController,
+            decoration: const InputDecoration(labelText: "Email"),
+          ),
+
+          //password
+          TextField(
+            controller: _passwordController,            
+            decoration: const InputDecoration(labelText: "Password"),
+          ),
+
+          //confirm password
+          TextField(
+            controller: _confirmPasswordController,            
+            decoration: const InputDecoration(labelText: "Confirm Password"),
+          ),
+
+
+          const SizedBox(height: 12), //spacing between the password field and the login button
+          //button 
+          ElevatedButton(onPressed: signUp, child: const Text("Sign Up")
+          ),
+          
+          //go to register page to sign up
+          GestureDetector(
+            onTap:() => Navigator.push(
+              context, 
+              MaterialPageRoute(builder: (context) => const LoginPage(),
+              )
+            ),
+            child:Center(child: Text("ALready have an account? sign in")),
+          )
+        ]
+      )
+    );
   }
 }
