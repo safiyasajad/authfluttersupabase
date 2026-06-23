dde3d1ee5c6aa1d87908203ffa23c6104e6d9f20
Safiya Sajad <safiyasajad@gmail.com>
2026-06-23 13:13:55 +0530

    Update profile_page.dart
    this page has a title, a log out button and will display the logged user's email

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,3 +1,4 @@
+import 'package:authfluttersupabase/auth/auth_service.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -8,8 +9,31 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  
+  //get auth service
+
+final authService = AuthService();
+
+  //logout button pressed
+  void logout() async{ 
+    await authService.signOut();  //set from auth_service page
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final currentEmail = authService.getCurrentUserEmail();
+   return Scaffold(
+      //the app bar gives the page a title as well as the back arrow on the left 
+      appBar: AppBar(
+        title: const Text("Profile Page"),
+        actions: [
+          //logout button
+          IconButton(onPressed: logout, 
+          icon: const Icon(Icons.logout))
+        ]
+      ),
+      //this will give me the loggedin user's email as the contnt of the page
+      body: Center(child: Text(currentEmail.toString()),)
+    );
   }
 }
