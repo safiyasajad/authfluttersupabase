02a7dd11f580a1699677f6e78748cdad90942bf6
Safiya Sajad <safiyasajad@gmail.com>
2026-06-24 13:06:04 +0530

    updates of the supabase project url and anon key

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,8 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 void main() async {
   //supabase setup
   await Supabase.initialize(
-    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBtdXl4Z2dkZWhqeHZzeGt0b3pyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE3NjE5MTYsImV4cCI6MjA5NzMzNzkxNn0.goLB5_R0gLW-4V3AZ2l-y-9KZxzbaOrDCVyCR_hb-Ms" ,
-    url: "https://pmuyxggdehjxvsxktozr.supabase.co");
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFudW55ZXRyaGhqZGJtcW1uZnFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyODEzMTYsImV4cCI6MjA5Nzg1NzMxNn0.ZYdpFt2YZmtEjHmEc9jicsIghHdp1PCs3wNEkH06yJ4" ,
+    url: "https://anunyetrhhjdbmqmnfqi.supabase.co");
     
 
   runApp(const MyApp());
